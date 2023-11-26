819f158b55fed427e98ed2c93b8427ea9bae27a0
malleron66 <malleron66@gmail.com>
2023-11-26 16:45:46 +0200

    newComit_1

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -1,5 +1,16 @@
 import Message from "../model/Message.js";
 
+export const getAll = async (req, res) => {
+  try {
+    
+    const message = await Message.find();
+    res.json(message);
+  } catch (error) {
+    console.error("Ошибка при обработке запроса:", error);
+    res.status(500).json({ error: "Не удалось получить сообщения." });
+  }
+};
+
 export const create = async (req, res) => {
   try {
     const doc = new Message({
